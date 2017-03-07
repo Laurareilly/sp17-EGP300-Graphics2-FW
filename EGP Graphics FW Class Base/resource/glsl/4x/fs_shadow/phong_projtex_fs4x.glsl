79554d45c88f75d1aebbc09171924e5c3b204829
laurareilly <laura.reilly@mymail.champlain.edu>
2017-03-06 21:09:15 -0500

    Shadow mapping

--- EGP Graphics FW Class Base/resource/glsl/4x/fs_shadow/phong_projtex_fs4x.glsl
+++ EGP Graphics FW Class Base/resource/glsl/4x/fs_shadow/phong_projtex_fs4x.glsl
@@ -10,11 +10,27 @@
 
 
 // ****
-// varyings: 
+// varyings:
+in vertexdata
+{
+	vec4 position_clip_alt;
+	vec4 normal;
+	vec4 texcoord;
+	vec4 lightVec;
+	vec4 eyeVec;
+
+} pass; 
 
 
 // ****
 // uniforms: 
+uniform sampler2D tex_dm;
+uniform sampler2D tex_sm;
+
+uniform sampler2D tex_bool;
+uniform sampler2D projtex;
+
+uniform vec4 lightColor;
 
 
 // target: 
@@ -23,9 +39,38 @@ layout (location = 0) out vec4 fragColor;
 
 // ****
 // constants/globals: 
-
+const vec4 AMBIENT = vec4(0.02, 0.01, 0.0, 0.0);
+vec2 booleanSample = vec2(0.0);
 
 void main()
 {
-	fragColor = vec4(1.0, 0.5, 0.0, 1.0);
+	vec4 projector_uv = pass.position_clip_alt / pass.position_clip_alt.w;
+	vec4 projectedTex = texture(projtex, projector_uv.xy);
+
+	booleanSample.x =pass.position_clip_alt.z + 1.0;
+	projectedTex *= texture(tex_bool, booleanSample);
+
+	vec4 diffuseColor = texture(tex_dm, pass.texcoord.xy);
+	vec4 specularColor = texture(tex_sm, pass.texcoord.xy);
+
+	vec4 N = normalize(pass.normal);
+	vec4 L = normalize(pass.lightVec);
+	float kd = dot(N, L);
+	
+	vec4 V = normalize(pass.eyeVec);
+	vec4 R = (kd + kd) * N - L;
+	float ks = dot(V, R);
+
+	kd = max(0.0, kd);
+	ks = max(0.0, ks);	
+
+	ks *= ks;
+	ks *= ks;
+	ks *= ks;
+	ks *= ks;
+	ks *= ks;
+	ks *= ks;
+	ks *= ks;
+	
+	fragColor = (diffuseColor * kd + specularColor * ks) * lightColor * projectedTex + AMBIENT;
 }
--- EGP Graphics FW Class Base/resource/glsl/4x/fs_shadow/phong_shadowmap_fs4x.glsl
+++ EGP Graphics FW Class Base/resource/glsl/4x/fs_shadow/phong_shadowmap_fs4x.glsl
@@ -11,11 +11,26 @@
 
 // ****
 // varyings: 
+in vertexdata
+{
+	vec4 position_clip_alt;
+	vec4 normal;
+	vec4 texcoord;
+	vec4 lightVec;
+	vec4 eyeVec;
+
+} pass; 
 
 
 // ****
 // uniforms: 
+uniform sampler2D tex_dm;
+uniform sampler2D tex_sm;
 
+uniform sampler2D tex_bool;
+uniform sampler2D shadowmap;
+
+uniform vec4 lightColor;
 
 // target: 
 layout (location = 0) out vec4 fragColor;
@@ -23,9 +38,42 @@ layout (location = 0) out vec4 fragColor;
 
 // ****
 // constants/globals: 
+const vec4 AMBIENT = vec4(0.02, 0.01, 0.0, 0.0);
+const float shadowBias = 0.0025;
+vec2 booleanSample = vec2(0.0);
 
 
 void main()
 {
-	fragColor = vec4(0.0, 0.5, 1.0, 1.0);
+	vec4 projector_uv = pass.position_clip_alt / pass.position_clip_alt.w;
+	float shadowSample = texture(shadowmap, projector_uv.xy).r;
+	bool fragIsShadowed = (projector_uv.z > (shadowSample + shadowBias));
+
+	booleanSample.x = pass.position_clip_alt.z + 1.0;
+
+	vec4 diffuseColor = texture(tex_dm, pass.texcoord.xy);
+	vec4 specularColor = texture(tex_sm, pass.texcoord.xy);
+
+	vec4 N = normalize(pass.normal);
+	vec4 L = normalize(pass.lightVec);
+	float kd = dot(N, L);
+	
+	if(fragIsShadowed && pass.position_clip_alt.z > 0)
+	{
+		kd *= 0.5;
+	}
+
+	vec4 V = normalize(pass.eyeVec);
+	vec4 R = (kd + kd) * N - L;
+	float ks = dot(V, R);
+
+	kd = max(0.0, kd);
+	ks = max(0.0, ks);	
+
+	ks *= ks;
+	ks *= ks;
+	ks *= ks;
+	ks *= ks;
+
+	fragColor = (diffuseColor * kd + specularColor * ks) * lightColor + AMBIENT;
 }
